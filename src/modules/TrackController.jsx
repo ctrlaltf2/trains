a6a1180fb127cd253154ee6c192a27e481beed54
chrisg564 <37278062+chrisg564@users.noreply.github.com>
2022-06-12 10:42:31 -0400

    Inital UI

--- src/modules/TrackController.jsx
+++
@@ -1,7 +0,0 @@
-const TrackController = () => {
-  return (
-    <p>Track Controller</p>
-  )
-};
-
-export default TrackController;
--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,50 +1,51 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import CTCOffice from '../modules/CTCOffice.tsx';
-import icon from '../../assets/icon.svg';
+// import CTCOffice from '../modules/CTCOffice.tsx';
+// import icon from '../../assets/icon.svg';
 import '../../assets/css/pico.min.css';
+import TrackController from 'modules/TrackController';
 
-const Hello = () => {
-  return (
-    <div>
-      <div className="Hello">
-        <img width="200px" alt="icon" src={icon} />
-      </div>
-      <h1>electron-react-boilerplate</h1>
-      <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
-      </div>
-    </div>
-  );
-};
+// const Hello = () => {
+//   return (
+//     <div>
+//       <div className="Hello">
+//         <img width="200px" alt="icon" src={icon} />
+//       </div>
+//       <h1>electron-react-boilerplate</h1>
+//       <div className="Hello">
+//         <a
+//           href="https://electron-react-boilerplate.js.org/"
+//           target="_blank"
+//           rel="noreferrer"
+//         >
+//           <button type="button">
+//             <span role="img" aria-label="books">
+//               📚
+//             </span>
+//             Read our docs
+//           </button>
+//         </a>
+//         <a
+//           href="https://github.com/sponsors/electron-react-boilerplate"
+//           target="_blank"
+//           rel="noreferrer"
+//         >
+//           <button type="button">
+//             <span role="img" aria-label="books">
+//               🙏
+//             </span>
+//             Donate
+//           </button>
+//         </a>
+//       </div>
+//     </div>
+//   );
+// };
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Hello />} />
+        <Route path="/" element={<TrackController />} />
       </Routes>
     </Router>
   );
