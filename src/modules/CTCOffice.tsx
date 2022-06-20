e2e969b8a464a484593f8c8b98fc08b84a2c93be
ctrlaltf2 <23644849+ctrlaltf2@users.noreply.github.com>
2022-06-19 22:35:10 -0400

    Modules do be communicating now

--- src/modules/CTCOffice.tsx
+++ src/modules/CTCOffice.tsx
@@ -1,5 +1,9 @@
+window.electronAPI.subscribeCTCMessage( (_event, payload) => {
+  console.log('IPC:CTCOffice: ', payload);
+});
+
 const CTCOffice = () => {
-  return <p>CTC Office!</p>;
+  return <p>CTC Office!!</p>;
 };
 
 export default CTCOffice;
