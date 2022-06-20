3fed8e22f98eff21fed8695654a37509bbe61514
ctrlaltf2 <23644849+ctrlaltf2@users.noreply.github.com>
2022-06-19 20:52:23 -0400

    Bring up windows for each module

--- src/main/preload.ts
+++ src/main/preload.ts
@@ -1,20 +1,30 @@
 import { contextBridge, ipcRenderer, IpcRendererEvent } from 'electron';
 
-contextBridge.exposeInMainWorld('electron', {
-  ipcRenderer: {
-    sendMessage(channel: Channels, args: unknown[]) {
-      ipcRenderer.send(channel, args);
-    },
-    on(channel: Channels, func: (...args: unknown[]) => void) {
-      const subscription = (_event: IpcRendererEvent, ...args: unknown[]) =>
-        func(...args);
-      ipcRenderer.on(channel, subscription);
+import * as Channels from './channels';
 
-      return () => ipcRenderer.removeListener(channel, subscription);
-    },
-    once(channel: Channels, func: (...args: unknown[]) => void) {
-      ipcRenderer.once(channel, (_event, ...args) => func(...args));
-    },
-    object: ipcRenderer,
-  },
+contextBridge.exposeInMainWorld('electronAPI', {
+  // Called by electron:main to send to this module
+  relayCTCMessage:              (callback) => ipcRenderer.on(Channels.CTC_OFFICE, callback),
+  relayTrackControllerMessage:  (callback) => ipcRenderer.on(Channels.TRACK_CONTROLLER, callback),
+  relayTrackModelMessage:       (callback) => ipcRenderer.on(Channels.TRACK_MODEL, callback),
+  relayTrainModelMessage:       (callback) => ipcRenderer.on(Channels.TRAIN_MODEL, callback),
+  relayTrainControllerMessage:  (callback) => ipcRenderer.on(Channels.TRAIN_CONTROLLER, callback),
+
+  // These will be called by the electron:renderer process for this module
+  // send*Message:    Asynchronously send a message (no response expected)
+  // receive*Message: Asynchronously send a message (and expect a response)
+  sendCTCMessage:                 (payload) => ipcRenderer.send(Channels.CTC_OFFICE, payload),
+  requestCTCMessage:              (payload) => ipcRenderer.invoke(Channels.CTC_OFFICE, payload),
+
+  sendTrackControllerMessage:     (payload) => ipcRenderer.send(Channels.TRACK_CONTROLLER, payload),
+  requestTrackControllerMessage:  (payload) => ipcRenderer.invoke(Channels.TRACK_CONTROLLER, payload),
+
+  sendTrackModelMessage:          (payload) => ipcRenderer.send(Channels.TRACK_MODEL, payload),
+  requestTrackModelMessage:       (payload) => ipcRenderer.invoke(Channels.TRACK_MODEL, payload),
+
+  sendTrainModelMessage:          (payload) => ipcRenderer.send(Channels.TRAIN_MODEL, payload),
+  requestTrainModelMessage:       (payload) => ipcRenderer.invoke(Channels.TRAIN_MODEL, payload),
+
+  sendTrainControllerMessage:     (payload) => ipcRenderer.send(Channels.TRAIN_CONTROLLER, payload),
+  requestTrainControllerMessage:  (payload) => ipcRenderer.invoke(Channels.TRAIN_CONTROLLER, payload),
 });
