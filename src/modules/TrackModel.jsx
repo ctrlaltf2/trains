9a8236db2f1675f4c7fd376f73603c27f0f61c50
cag185 <cag185@pitt.edu>
2022-06-24 16:13:20 -0400

    Adding more UI

--- src/modules/TrackModel.jsx
+++ src/modules/TrackModel.jsx
@@ -6,13 +6,24 @@ import
   Container,
   TextField
 } from '@mui/material/';
+import { blue, grey } from "@mui/material/colors";
+import { Box } from "@mui/system";
 
 const TrackModel = () => {
   return (
-        <Container maxWidth="lg">
-        <h1>
-          Track Controller UI
-        </h1>
+    <Container maxWidth="lg">
+
+      <div className="Center">
+      <p1>Track Controller UI</p1>
+      </div>
+
+      <Box className="TopBox"
+      sx={{width: 1000, height: 150, backgroundColor:'rgb(225,225,225)', border: '5px dotted black', justifySelf: "center"}}
+      >
+      <Button className ="LoadTrack">Load Track</Button>
+      <Button className = "RestoreDefaults">Restore to Default Settings</Button>
+      </Box>
+
         </Container>
   )
 };
