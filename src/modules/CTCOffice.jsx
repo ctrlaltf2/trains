0e8eaa22a8ec40f1c7be0f31e857d4684431912a
ctrlaltf2 <ctrlaltf2@protonmail.ch>
2022-06-26 21:56:44 -0400

    Get IPC <-> React component state working

--- src/modules/CTCOffice.jsx
+++ src/modules/CTCOffice.jsx
@@ -1,21 +1,57 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-window.electronAPI.subscribeCTCMessage( (_event, payload) => {
-  console.log('IPC:CTCOffice: ', payload);
-});
+/*
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});*/
+
+import './CTCOffice.css';
+
 
 class CTCOffice extends React.Component {
   constructor(props) {
+    window.electronAPI.subscribeCTCMessage( (_event, payload) => {
+      console.log('IPC:CTCOffice: ', payload);
+
+      switch(payload.type) {
+        case 'setTestMode':
+          this.setState({
+            testMode: payload.value
+          });
+
+          console.log('Set the test mode to', payload.value);
+          break;
+        case 'callAFunction':
+          this.test();
+        default:
+          console.warn('Unknown payload type received: ', payload.type);
+      }
+    });
+
     super(props);
     this.state = {
-      testMode: false,
+      testMode: true,
     };
   }
 
+  test() {
+    console.log('abcdef');
+  }
+
+
   testUI() {
     return (
-      <p>CTC Office Test UI</p>
+      <div className="testUIContainer">
+        <div className="inputContainer">
+          <h4 className="containerTitle">From Track Controller (Inputs to module)</h4>
+        </div>
+        <div className="outputContainer">
+          <h4 className="containerTitle">To Track Controller (Outputs from module)</h4>
+        </div>
+      </div>
     );
   }
 
@@ -24,7 +60,7 @@ class CTCOffice extends React.Component {
       return this.testUI();
 
     return (
-      <p>I just want to graduate already</p>
+      <p>CTC Office UI</p>
     );
   }
 };
