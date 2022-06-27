1758004270dcca530bf2a9f2a37316425a101657
ctrlaltf2 <23644849+ctrlaltf2@users.noreply.github.com>
2022-06-26 22:46:23 -0400

    Add main render states for main user actions

--- src/modules/CTCOffice.jsx
+++ src/modules/CTCOffice.jsx
@@ -1,15 +1,30 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-/*
+import { ThemeProvider, createTheme } from '@mui/material';
+
+import './CTCOffice.css';
+
 const darkTheme = createTheme({
   palette: {
     mode: 'dark',
   },
-});*/
+});
 
-import './CTCOffice.css';
-
+// Enum-like structure for all UI states
+const UIState = {
+  Main:         'Main',
+  Dispatching:  'Dispatching',
+  Scheduling:   'Scheduling',
+  Viewing: {
+    Train:      'ViewingTrain',
+    BlueLine:   'ViewingBlueLine',
+    RedLine:    'ViewingRedLine',
+    GreenLine:  'ViewingGreenLine',
+    Map:        'Map',
+  },
+  Test:         'Test',
+};
 
 class CTCOffice extends React.Component {
   constructor(props) {
@@ -17,51 +32,61 @@ class CTCOffice extends React.Component {
       console.log('IPC:CTCOffice: ', payload);
 
       switch(payload.type) {
-        case 'setTestMode':
-          this.setState({
-            testMode: payload.value
-          });
-
-          console.log('Set the test mode to', payload.value);
-          break;
-        case 'callAFunction':
-          this.test();
         default:
           console.warn('Unknown payload type received: ', payload.type);
       }
     });
 
     super(props);
+
     this.state = {
       testMode: true,
+      UIState: UIState.Main
     };
   }
 
-  test() {
-    console.log('abcdef');
+  renderTest() {
+    return (
+      <ThemeProvider theme={darkTheme}>
+        <div className="testUIContainer">
+          <div className="inputContainer">
+            <h4 className="containerTitle">From Track Controller (Inputs to module)</h4>
+          </div>
+          <div className="outputContainer">
+            <h4 className="containerTitle">To Track Controller (Outputs from module)</h4>
+          </div>
+        </div>
+      </ThemeProvider>
+    );
   }
 
-
-  testUI() {
+  renderMain() {
     return (
-      <div className="testUIContainer">
-        <div className="inputContainer">
-          <h4 className="containerTitle">From Track Controller (Inputs to module)</h4>
-        </div>
-        <div className="outputContainer">
-          <h4 className="containerTitle">To Track Controller (Outputs from module)</h4>
-        </div>
-      </div>
+      <ThemeProvider theme={darkTheme}>
+        <p>Je voudrais pour abandonner d'universite</p>
+      </ThemeProvider>
     );
   }
 
   render() {
-    if(this.state.testMode)
-      return this.testUI();
-
-    return (
-      <p>CTC Office UI</p>
-    );
+    switch(this.state.UIState) {
+      case UIState.Main:
+        return this.renderMain();
+      case UIState.Test:
+        return this.renderTest();
+      case UIState.Dispatching:
+      case UIState.Scheduling:
+      case UIState.Viewing.Train:
+      case UIState.Viewing.BlueLine:
+      case UIState.Map:
+      case UIState.Viewing.RedLine:
+      case UIState.Viewing.GreenLine:
+      default:
+        console.warn('Unimplemented UI state: ', this.state.UIState);
+        return (
+          <p>Unimplemented UI state of {this.state.UIState}</p>
+        );
+    }
   }
 };
 
