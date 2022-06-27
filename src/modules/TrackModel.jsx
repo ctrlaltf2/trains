176206460a478c802f5d5f02f9cfb0452df56d30
cag185 <cag185@pitt.edu>
2022-06-27 01:01:25 -0400

    Added tons of buttons to UI

--- src/modules/TrackModel.jsx
+++ src/modules/TrackModel.jsx
@@ -4,6 +4,7 @@ import
 {
   Button,
   Container,
+  Grid,
   TextField
 } from '@mui/material/';
 import { blue, grey } from "@mui/material/colors";
@@ -12,19 +13,122 @@ import { Box } from "@mui/system";
 const TrackModel = () => {
   return (
     <Container maxWidth="lg">
+      <Grid
+        container
+        spacing={12}
+        direction="row"
+        justifyContent="center">
+        <Grid item xs={3} justifyContent="center">
+          <div className="HeaderText"  >Track Model</div>
+        </Grid>
+      </Grid>
+      <Grid container spacing={1} direction="row">
+        <Grid item xs={4}>
+          <Button className="LoadTrack">Load new Track Model</Button>
+        </Grid>
+        <Grid item xs={4}>
+          <></>
+        </Grid>
+        <Grid item xs={4}>
+          <Button className="RestoreDefaults">Reset to Default Settings</Button>
+        </Grid>
+      </Grid>
 
-      <div className="Center">
-      <p1>Track Controller UI</p1>
-      </div>
+        {/* Need three columns, and in the far right column a grid with two columns */}
+      <Grid
+        container
+        spacing={12}
+        direction="row"
+        justifyContent="center">
 
-      <Box className="TopBox"
-      sx={{width: 1000, height: 150, backgroundColor:'rgb(225,225,225)', border: '5px dotted black', justifySelf: "center"}}
-      >
-      <Button className ="LoadTrack">Load Track</Button>
-      <Button className = "RestoreDefaults">Restore to Default Settings</Button>
-      </Box>
+        {/* Column 1 */}
+        <Grid item xs={4}>
+          <Grid
+            container
+            spacing={12}>
+          <></>
+          </Grid>
+        </Grid>
 
-        </Container>
+        {/* Column 2 */}
+        <Grid item xs={4}>
+          <Grid
+            container
+            spacing={12} >
+           <></>
+          </Grid>
+        </Grid>
+
+        {/* Column 3 */}
+        <Grid item xs={4}>
+        <Grid
+          container
+          spacing={1}
+          direction="row"
+          justifyContent="center">
+
+          <Grid item xs={6}>
+            <Button>Fail Track Power</Button>
+          </Grid>
+
+          <Grid item xs={6}>
+            <Button>Reset Power</Button>
+          </Grid>
+
+          <Grid item xs={6}>
+            <Button>Break Rail</Button>
+          </Grid>
+
+          <Grid item xs={6}>
+            <Button>Reset Rail</Button>
+          </Grid>
+
+          <Grid item xs={6}>
+            <Button>Stop Sending Track Circuit</Button>
+          </Grid>
+
+          <Grid item xs={6}>
+            <Button>Reset Sending Track Circuit</Button>
+          </Grid>
+
+          <Grid item xs={6}>
+            <Button>Stop Recieveing Track Cicuit</Button>
+          </Grid>
+
+          <Grid item xs={6}>
+            <Button>Reset Recieveing Track Cicuit</Button>
+          </Grid>
+
+
+          <Grid item xs={6}>
+            <Button>Change Elevation</Button>
+          </Grid>
+
+          <Grid item xs={6}>
+            <Button>Reset Elevation</Button>
+          </Grid>
+
+          <Grid item xs={6}>
+            <Button>Stop Beacon</Button>
+          </Grid>
+
+          <Grid item xs={6}>
+            <Button>Reset Beacon</Button>
+          </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+        {/* Resets for all the buttons */}
+
+        {/*
+
+
+        <Button>Reset Elevation</Button>
+        <Button>Reset Switch Position</Button>
+        <Button>Reset Beacon</Button> */}
+
+
+    </Container>
   )
 };
 
