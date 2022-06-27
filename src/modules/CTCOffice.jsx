b0efeaf933b19448a05f84eae54c1372a94c6452
ctrlaltf2 <23644849+ctrlaltf2@users.noreply.github.com>
2022-06-27 00:52:18 -0400

    Add navigation between remaining UI modes

--- src/modules/CTCOffice.jsx
+++ src/modules/CTCOffice.jsx
@@ -56,7 +56,7 @@ class CTCOffice extends React.Component {
           </div>
           <div className="outputContainer">
             <h4 className="containerTitle">To Track Controller (Outputs from module)</h4>
-            <div className="grow-divider"></div>
+            <div className="grow-divider"/>
             <Button variant="contained" onClick={() => {
               this.setState({UIMode: UIState.Main});
             }}>
@@ -68,15 +68,72 @@ class CTCOffice extends React.Component {
     );
   }
 
+  renderDispatch() {
+    return (
+      <ThemeProvider theme={darkTheme}>
+        <Button className="backButton" variant="contained" onClick={() => {
+          this.setState({UIMode: UIState.Main});
+        }}>
+          Return to dashboard
+        </Button>
+        <div className="dispatchContainer">
+
+        </div>
+      </ThemeProvider>
+    );
+  }
+
+  renderMap() {
+    return (
+      <ThemeProvider theme={darkTheme}>
+        <Button className="backButton" variant="contained" onClick={() => {
+          this.setState({UIMode: UIState.Main});
+        }}>
+          Return to dashboard
+        </Button>
+        <div className="mapContainer">
+
+        </div>
+      </ThemeProvider>
+    );
+  }
+
+  renderScheduler() {
+    return (
+      <ThemeProvider theme={darkTheme}>
+        <Button className="backButton" variant="contained" onClick={() => {
+          this.setState({UIMode: UIState.Main});
+        }}>
+          Return to dashboard
+        </Button>
+        <div className="schedulerContainer">
+
+        </div>
+      </ThemeProvider>
+    );
+  }
+
   renderMain() {
     return (
       <ThemeProvider theme={darkTheme}>
         <div className="mainContainer">
           <h1>What would you like to do?</h1>
-          <Button variant="contained">Dispatch Train</Button>
-          <Button variant="contained">View System Map</Button>
-          <Button variant="contained">Load System Schedule</Button>
-          <div className="grow-divider"></div>
+          <Button variant="contained" onClick={() => {
+              this.setState({UIMode: UIState.Dispatching});
+          }}>
+            Dispatch Train
+          </Button>
+          <Button variant="contained" onClick={() => {
+              this.setState({UIMode: UIState.Map});
+          }}>
+            View System Map
+          </Button>
+          <Button variant="contained" onClick={() => {
+              this.setState({UIMode: UIState.Scheduling});
+          }}>
+            Load System Schedule
+          </Button>
+          <div className="grow-divider"/>
           <Button variant="contained" onClick={() => {
             this.setState({UIMode: UIState.Test});
           }}>
@@ -96,10 +153,13 @@ class CTCOffice extends React.Component {
       case UIState.Test:
         return this.renderTest();
       case UIState.Dispatching:
+        return this.renderDispatch();
       case UIState.Scheduling:
+        return this.renderScheduler();
+      case UIState.Map:
+        return this.renderMap();
       case UIState.Viewing.Train:
       case UIState.Viewing.BlueLine:
-      case UIState.Map:
       case UIState.Viewing.RedLine:
       case UIState.Viewing.GreenLine:
       default:
