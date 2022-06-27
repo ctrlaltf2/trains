f9b27f6b7fbe8a251da034cd747a80c1b430f76b
ctrlaltf2 <ctrlaltf2@protonmail.ch>
2022-06-26 21:39:36 -0400

    Remove old tsx file

--- src/modules/CTCOffice.tsx
+++
@@ -1,9 +0,0 @@
-window.electronAPI.subscribeCTCMessage( (_event, payload) => {
-  console.log('IPC:CTCOffice: ', payload);
-});
-
-const CTCOffice = () => {
-  return <p>CTC Office!!</p>;
-};
-
-export default CTCOffice;
