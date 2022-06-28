66639651373c579e523e8a4b8aa6a834776d6abe
Cameron Henning <cwh35@pitt.edu>
2022-06-27 23:03:41 -0400

    Added toggling to failures and E brake

--- src/modules/TrainControllerSW.jsx
+++ src/modules/TrainControllerSW.jsx
@@ -1,4 +1,5 @@
-import{
+import React, { useState } from 'react';
+import {
   styled,
   Box,
   Paper,
@@ -8,8 +9,11 @@ import{
   Switch,
   Button,
   Slider,
-  Stack
-} from "@mui/material";
+  Stack,
+  AppBar,
+  Typography,
+  Toolbar
+} from '@mui/material';
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -24,110 +28,351 @@ function preventHorizontalKeyboardNavigation(event) {
   }
 }
 
-const TrainControllerSW = () => {
-  return (
-<Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={4}>
-        <Grid item xs={3} md={8}>
-          <Item>
-            <FormGroup>
-              <FormControlLabel control={<Switch defaultChecked />} label="Left Train Doors" />
-            </FormGroup>
-          </Item>
-        </Grid>
-        <Grid item xs={3} md={4}>
-          <Item>
-            <FormGroup>
-              <FormControlLabel control={<Switch defaultChecked />} label="Right Train Doors" />
-            </FormGroup>
-          </Item>
-        </Grid>
-        <Grid item xs={3} md={4}>
-          <Item>
-            <FormGroup>
-              <FormControlLabel control={<Switch defaultChecked />} label="Cabin Lights" />
-            </FormGroup>
-          </Item>
-        </Grid>
-        <Grid item xs={3} md={8}>
-          <Item>
-            <FormGroup>
-              <FormControlLabel control={<Switch defaultChecked />} label="Train Lights" />
-            </FormGroup>
-          </Item>
-        </Grid>
-        <Grid item xs={5} md={8}>
-          <Item>
-            <FormGroup>
-              <FormControlLabel control={<Switch defaultChecked />} label="Automatic/Manual Mode" />
-            </FormGroup>
-          </Item>
-        </Grid>
-        <Grid item xs={6} md={8}>
-          <Item>
-            <Button variant="outlined" color="error">
-              Emergency Brake
-            </Button>
-          </Item>
-        </Grid>
-        <Grid item xs={4} md={2}>
-          <Item>Power: _ Watts</Item>
-        </Grid>
-        <Grid item xs={6} md={8}>
-          <Item>Cabin Temperature</Item>
-        </Grid>
-        <Box sx={{ height: 300 }}>
-          <Slider
-            sx={{
-              '& input[type="range"]': {
-                WebkitAppearance: 'slider-vertical',
-              },
-            }}
-            orientation="vertical"
-            defaultValue={30}
-            aria-label="Temperature"
-            valueLabelDisplay="auto"
-            onKeyDown={preventHorizontalKeyboardNavigation}
-          />
-        </Box>
-        <Box sx={{ height: 300 }}>
-          <Slider
-            sx={{
-              '& input[type="range"]': {
-                WebkitAppearance: 'slider-vertical',
-              },
-            }}
-            orientation="vertical"
-            defaultValue={30}
-            aria-label="Temperature"
-            valueLabelDisplay="auto"
-            onKeyDown={preventHorizontalKeyboardNavigation}
-          />
-        </Box>
-        <Grid item xs={4} md={2}>
-          <Item>SPEED: _ MPH</Item>
-        </Grid>
-        <Grid item xs={4} md={2}>
-          <Item>Commanded Speed: _ MPH</Item>
-        </Grid>
-        <Grid item xs={4} md={2}>
-          <Item>Authority: _ Miles</Item>
-        </Grid>
-        <Grid item xs={5} md={2}>
-          <Item>Next Stop: _</Item>
-        </Grid>
-        <Stack
-          direction={{ xs: 'column', sm: 'row' }}
-          spacing={0}
-        >
-          <Item>Engine Failure</Item>
-          <Item>Brake Failure</Item>
-          <Item>Signal Pickup Failure</Item>
-      </Stack>
-      </Grid>
-    </Box>
+class TrainControllerSW extends React.Component {
+  constructor(props, name) {
+    super(props);
+    this.name = name;
 
-  )
-};
+    this.state = {
+      testMode: false,
+      emergencyButton: false,
+      brakeFailureDisplay: false,
+      engineFailureDisplay: false,
+      signalPickupFailureDisplay: false,
+    };
+
+    this.toggle = this.toggle.bind(this);
+    this.emergencyBrake = this.emergencyBrake.bind(this);
+    this.brakeFailure = this.brakeFailure.bind(this);
+    this.engineFailure = this.engineFailure.bind(this);
+    this.signalPickupFailure = this.signalPickupFailure.bind(this);
+  }
+
+  toggle() { // Toggles between regular UI and Test UI
+    this.setState((prevState) => ({
+      testMode: !prevState.testMode,
+    }));
+  }
+
+  emergencyBrake(){ // Toggles the emergency brake in the Test UI
+    this.setState((prevState) => ({
+      emergencyButton: !prevState.emergencyButton,
+    }));
+  }
+
+  brakeFailure(){ // Toggles the break failure display in the Test UI
+    this.setState((prevState) => ({
+      brakeFailureDisplay: !prevState.brakeFailureDisplay,
+    }));
+  }
+
+  engineFailure(){ // Toggles the engine failure display in the Test UI
+    this.setState((prevState) => ({
+      engineFailureDisplay: !prevState.engineFailureDisplay,
+    }));
+  }
+
+  signalPickupFailure(){ // Toggles the signal pickup failure display in the Test UI
+    this.setState((prevState) => ({
+      signalPickupFailureDisplay: !prevState.signalPickupFailureDisplay,
+    }));
+  }
+
+  testUI() {
+
+    return (
+
+      <Box sx={{ flexGrow: 1 }}>
+        <Box sx={{ flexGrow: 1 }}>
+          <AppBar position="static">
+            <Toolbar variant="dense">
+              <Typography variant="h6" color="white" component="div">
+                Train Controller Test UI
+              </Typography>
+            </Toolbar>
+          </AppBar>
+        </Box>
+        <Grid container spacing={4}>
+          <Grid item xs={3} md={8}>
+            <Item>
+              <FormGroup>
+                <FormControlLabel
+                  control={<Switch defaultChecked />}
+                  label="Left Train Doors"
+                />
+              </FormGroup>
+            </Item>
+          </Grid>
+          <Grid item xs={3} md={4}>
+            <Item>
+              <FormGroup>
+                <FormControlLabel
+                  control={<Switch defaultChecked />}
+                  label="Right Train Doors"
+                />
+              </FormGroup>
+            </Item>
+          </Grid>
+          <Grid item xs={3} md={4}>
+            <Item>
+              <FormGroup>
+                <FormControlLabel
+                  control={<Switch defaultChecked />}
+                  label="Cabin Lights"
+                />
+              </FormGroup>
+            </Item>
+          </Grid>
+          <Grid item xs={3} md={8}>
+            <Item>
+              <FormGroup>
+                <FormControlLabel
+                  control={<Switch defaultChecked />}
+                  label="Train Lights"
+                />
+              </FormGroup>
+            </Item>
+          </Grid>
+          <Grid item xs={5} md={8}>
+            <Item>
+              <FormGroup>
+                <FormControlLabel
+                  control={<Switch defaultChecked />}
+                  label="Automatic/Manual Mode"
+                />
+              </FormGroup>
+            </Item>
+          </Grid>
+          <Grid item xs={6} md={8}>
+            <Item>
+              {this.state.emergencyButton ? (
+              <Button variant="contained" color="error" onClick={this.emergencyBrake}>
+                Emergency Brake Activated
+              </Button>
+              ) : (
+              <Button variant="outlined" color="error" onClick={this.emergencyBrake}>
+                Emergency Brake Deactivated
+              </Button>
+              )}
+            </Item>
+          </Grid>
+          <Grid item xs={4} md={2}>
+            <Item>Power: _ Watts</Item>
+          </Grid>
+          <Grid item xs={6} md={8}>
+            <Item>Cabin Temperature</Item>
+          </Grid>
+          <Box sx={{ height: 300 }}>
+            <Slider
+              sx={{
+                '& input[type="range"]': {
+                  WebkitAppearance: 'slider-vertical',
+                },
+              }}
+              orientation="vertical"
+              defaultValue={30}
+              aria-label="Temperature"
+              valueLabelDisplay="auto"
+              onKeyDown={preventHorizontalKeyboardNavigation}
+            />
+          </Box>
+          <Box sx={{ height: 300 }}>
+            <Slider
+              sx={{
+                '& input[type="range"]': {
+                  WebkitAppearance: 'slider-vertical',
+                },
+              }}
+              orientation="vertical"
+              defaultValue={30}
+              aria-label="Temperature"
+              valueLabelDisplay="auto"
+              onKeyDown={preventHorizontalKeyboardNavigation}
+            />
+          </Box>
+          <Grid item xs={4} md={2}>
+            <Item>SPEED: _ MPH</Item>
+          </Grid>
+          <Grid item xs={4} md={2}>
+            <Item>Commanded Speed: _ MPH</Item>
+          </Grid>
+          <Grid item xs={4} md={2}>
+            <Item>Authority: _ Miles</Item>
+          </Grid>
+          <Grid item xs={5} md={2}>
+            <Item>Next Stop: _</Item>
+          </Grid>
+          <Stack direction={{ xs: 'column', sm: 'row' }} spacing={0.5}>
+            {this.state.brakeFailureDisplay ? (
+              <Button variant="contained" color="error" onClick={this.brakeFailure}>
+                Brake Status: Failing
+              </Button>
+              ) : (
+              <Button variant="contained" color="success" onClick={this.brakeFailure}>
+                Brake Status: Working
+              </Button>
+              )}
+            {this.state.engineFailureDisplay ? (
+              <Button variant="contained" color="error" onClick={this.engineFailure}>
+                Engine Status: Failing
+              </Button>
+              ) : (
+              <Button variant="contained" color="success" onClick={this.engineFailure}>
+                Engine Status: Working
+              </Button>
+              )}
+            {this.state.signalPickupFailureDisplay ? (
+              <Button variant="contained" color="error" onClick={this.signalPickupFailure}>
+                Signal Pickup Status: Error
+              </Button>
+              ) : (
+              <Button variant="contained" color="success" onClick={this.signalPickupFailure}>
+                Signal Status: Strong
+              </Button>
+              )}
+          </Stack>
+          <Stack spacing={2} direction="row">
+            <Button variant="contained" onClick={this.toggle}>
+              Toggle Test UI
+            </Button>
+          </Stack>
+        </Grid>
+      </Box>
+    );
+  }
+
+  render() {
+    if (this.state.testMode) return this.testUI();
+
+    return (
+      <Box sx={{ flexGrow: 1 }}>
+        <Box sx={{ flexGrow: 1 }}>
+          <AppBar position="static">
+            <Toolbar variant="dense">
+              <Typography variant="h6" color="inherit" component="div">
+                Train Controller UI
+              </Typography>
+            </Toolbar>
+          </AppBar>
+        </Box>
+        <Grid container spacing={4}>
+          <Grid item xs={3} md={8}>
+            <Item>
+              <FormGroup>
+                <FormControlLabel
+                  control={<Switch defaultChecked />}
+                  label="Left Train Doors"
+                />
+              </FormGroup>
+            </Item>
+          </Grid>
+          <Grid item xs={3} md={4}>
+            <Item>
+              <FormGroup>
+                <FormControlLabel
+                  control={<Switch defaultChecked />}
+                  label="Right Train Doors"
+                />
+              </FormGroup>
+            </Item>
+          </Grid>
+          <Grid item xs={3} md={4}>
+            <Item>
+              <FormGroup>
+                <FormControlLabel
+                  control={<Switch defaultChecked />}
+                  label="Cabin Lights"
+                />
+              </FormGroup>
+            </Item>
+          </Grid>
+          <Grid item xs={3} md={8}>
+            <Item>
+              <FormGroup>
+                <FormControlLabel
+                  control={<Switch defaultChecked />}
+                  label="Train Lights"
+                />
+              </FormGroup>
+            </Item>
+          </Grid>
+          <Grid item xs={5} md={8}>
+            <Item>
+              <FormGroup>
+                <FormControlLabel
+                  control={<Switch defaultChecked />}
+                  label="Automatic/Manual Mode"
+                />
+              </FormGroup>
+            </Item>
+          </Grid>
+          <Grid item xs={6} md={8}>
+            <Item>
+              <Button variant="outlined" color="error">
+                Emergency Brake
+              </Button>
+            </Item>
+          </Grid>
+          <Grid item xs={4} md={2}>
+            <Item>Power: _ Watts</Item>
+          </Grid>
+          <Grid item xs={6} md={8}>
+            <Item>Cabin Temperature</Item>
+          </Grid>
+          <Box sx={{ height: 300 }}>
+            <Slider
+              sx={{
+                '& input[type="range"]': {
+                  WebkitAppearance: 'slider-vertical',
+                },
+              }}
+              orientation="vertical"
+              defaultValue={30}
+              aria-label="Temperature"
+              valueLabelDisplay="auto"
+              onKeyDown={preventHorizontalKeyboardNavigation}
+            />
+          </Box>
+          <Box sx={{ height: 300 }}>
+            <Slider
+              sx={{
+                '& input[type="range"]': {
+                  WebkitAppearance: 'slider-vertical',
+                },
+              }}
+              orientation="vertical"
+              defaultValue={30}
+              aria-label="Temperature"
+              valueLabelDisplay="auto"
+              onKeyDown={preventHorizontalKeyboardNavigation}
+            />
+          </Box>
+          <Grid item xs={4} md={2}>
+            <Item>SPEED: _ MPH</Item>
+          </Grid>
+          <Grid item xs={4} md={2}>
+            <Item>Commanded Speed: _ MPH</Item>
+          </Grid>
+          <Grid item xs={4} md={2}>
+            <Item>Authority: _ Miles</Item>
+          </Grid>
+          <Grid item xs={5} md={2}>
+            <Item>Next Stop: _</Item>
+          </Grid>
+          <Stack direction={{ xs: 'column', sm: 'row' }} spacing={0.5}>
+            <Item>Engine Failure</Item>
+            <Item>Brake Failure</Item>
+            <Item>Signal Pickup Failure</Item>
+          </Stack>
+          <Stack spacing={2} direction="row">
+            <Button variant="contained" onClick={this.toggle}>
+              Toggle Test UI
+            </Button>
+          </Stack>
+        </Grid>
+      </Box>
+    );
+  }
+}
 
 export default TrainControllerSW;
