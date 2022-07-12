4d3a97bb50b2c010a8077f73f19c2d638c8f88de
chrisg564 <37278062+chrisg564@users.noreply.github.com>
2022-07-12 09:37:54 -0400

    new switch logic

--- src/modules/PLCReader.ts
+++ src/modules/PLCReader.ts
@@ -1,5 +1,5 @@
 export class PLCReader {
-  public file:JSON;
+  public file: JSON;
 
   public line: string;
 
@@ -11,29 +11,29 @@ export class PLCReader {
   }
 
   // Parse PLC file into useful logic
-  parse (){
+  parse() {
     for (const key in this.file) {
       if (this.file[key].command === 'switch') {
+        console.log('yes');
         // store arg and logic for switch
         let i = 1;
         let arg = `arg${String(i)}`;
         let op = `op${String(i)}`;
         const switchLogic = [];
         while (true) {
-          if (this.file.hasOwnProperty(arg)){
+          if (this.file[key].hasOwnProperty(arg)) {
             switchLogic.push(this.file[key][arg]);
-          } else if (this.file.hasOwnProperty(op)){
+          } if (this.file[key].hasOwnProperty(op)) {
             switchLogic.push(this.file[key][op]);
           } else {
             break;
           }
           i += 1;
-          op = `op${String(i)}`
+          op = `op${String(i)}`;
           arg = `arg${String(i)}`;
         }
 
-        console.log(switchLogic);
-
+        console.log('logic' + switchLogic);
       }
       // if (file[key].command === 'cross') {
       //   if f
