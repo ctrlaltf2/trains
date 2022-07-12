081cde36d4e0c944f4a576fa9c26b65b42a1c448
chrisg564 <37278062+chrisg564@users.noreply.github.com>
2022-07-12 13:07:34 -0400

    improve plc

--- src/modules/PLCReader.ts
+++ src/modules/PLCReader.ts
@@ -1,6 +1,11 @@
 export class PLCReader {
   public file: JSON;
-
+  public switchLogic: [];
+  public crossingLogic: [];
+  public redLogic: [];
+  public yellowLogic: [];
+  public greenLogic: [];
+  public authorityLogic: [];
   public line: string;
 
   constructor(file: JSON, line: string) {
@@ -13,18 +18,20 @@ export class PLCReader {
   // Parse PLC file into useful logic
   parse() {
     for (const key in this.file) {
+      /*
+       *  switch logic
+       */
       if (this.file[key].command === 'switch') {
-        console.log('yes');
         // store arg and logic for switch
         let i = 1;
         let arg = `arg${String(i)}`;
         let op = `op${String(i)}`;
-        const switchLogic = [];
         while (true) {
           if (this.file[key].hasOwnProperty(arg)) {
-            switchLogic.push(this.file[key][arg]);
-          } if (this.file[key].hasOwnProperty(op)) {
-            switchLogic.push(this.file[key][op]);
+            this.switchLogic.push(this.file[key][arg]);
+          }
+          if (this.file[key].hasOwnProperty(op)) {
+            this.switchLogic.push(this.file[key][op]);
           } else {
             break;
           }
@@ -35,9 +42,90 @@ export class PLCReader {
 
         console.log('logic' + switchLogic);
       }
-      // if (file[key].command === 'cross') {
-      //   if f
-      // }
+      /*
+       *  crossing logic
+       */
+      if (this.file[key].command === 'cross') {
+        let i = 1;
+        let arg = `arg${String(i)}`;
+        let op = `op${String(i)}`;
+        while (true) {
+          if (this.file[key].hasOwnProperty(arg)) {
+            this.crossingLogic.push(this.file[key][arg]);
+          }
+          if (this.file[key].hasOwnProperty(op)) {
+            this.crossingLogic.push(this.file[key][op]);
+          } else {
+            break;
+          }
+          i += 1;
+          op = `op${String(i)}`;
+          arg = `arg${String(i)}`;
+        }
+      }
+      /*
+       *  red transit light
+       */
+      if (this.file[key].command === 'red') {
+        let i = 1;
+        let arg = `arg${String(i)}`;
+        let op = `op${String(i)}`;
+        while (true) {
+          if (this.file[key].hasOwnProperty(arg)) {
+            this.redLogic.push(this.file[key][arg]);
+          }
+          if (this.file[key].hasOwnProperty(op)) {
+            this.redLogic.push(this.file[key][op]);
+          } else {
+            break;
+          }
+          i += 1;
+          op = `op${String(i)}`;
+          arg = `arg${String(i)}`;
+        }
+      }
+      /*
+       *  yellow transit light
+       */
+      if (this.file[key].command === 'yellow') {
+        let i = 1;
+        let arg = `arg${String(i)}`;
+        let op = `op${String(i)}`;
+        while (true) {
+          if (this.file[key].hasOwnProperty(arg)) {
+            this.yellowLogic.push(this.file[key][arg]);
+          }
+          if (this.file[key].hasOwnProperty(op)) {
+            this.yellowLogic.push(this.file[key][op]);
+          } else {
+            break;
+          }
+          i += 1;
+          op = `op${String(i)}`;
+          arg = `arg${String(i)}`;
+        }
+      }
+      /*
+       *  green transit light
+       */
+      if (this.file[key].command === 'green') {
+        let i = 1;
+        let arg = `arg${String(i)}`;
+        let op = `op${String(i)}`;
+        while (true) {
+          if (this.file[key].hasOwnProperty(arg)) {
+            this.greenLogic.push(this.file[key][arg]);
+          }
+          if (this.file[key].hasOwnProperty(op)) {
+            this.greenLogic.push(this.file[key][op]);
+          } else {
+            break;
+          }
+          i += 1;
+          op = `op${String(i)}`;
+          arg = `arg${String(i)}`;
+        }
+      }
     }
   }
 }
