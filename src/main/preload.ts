1f7c9f9b141ad49ea46f5368e5276c6bdaaca5aa
ctrlaltf2 <23644849+ctrlaltf2@users.noreply.github.com>
2022-07-14 10:48:14 -0400

    Implement file loading functionality

--- src/main/preload.ts
+++ src/main/preload.ts
@@ -10,24 +10,17 @@ contextBridge.exposeInMainWorld('electronAPI', {
   subscribeTrainModelMessage:       (callback) => ipcRenderer.on(Modules.TRAIN_MODEL, callback),
   subscribeTrainControllerMessage:  (callback) => ipcRenderer.on(Modules.TRAIN_CONTROLLER, callback),
 
+  subscribeFileMessage: (callback) => ipcRenderer.on('file', callback),
+
   // These will be called by the electron:renderer process for this module
   // send*Message:    Asynchronously send a message (no response expected)
   // receive*Message: Asynchronously send a message (and expect a response)
   sendCTCMessage:                 (payload) => ipcRenderer.send(Modules.CTC_OFFICE, payload),
-  requestCTCMessage:              (payload) => ipcRenderer.invoke(Modules.CTC_OFFICE, payload),
-
   sendTrackControllerMessage:     (payload) => ipcRenderer.send(Modules.TRACK_CONTROLLER, payload),
-  requestTrackControllerMessage:  (payload) => ipcRenderer.invoke(Modules.TRACK_CONTROLLER, payload),
-
   sendTrackModelMessage:          (payload) => ipcRenderer.send(Modules.TRACK_MODEL, payload),
-  requestTrackModelMessage:       (payload) => ipcRenderer.invoke(Modules.TRACK_MODEL, payload),
-
   sendTrainModelMessage:          (payload) => ipcRenderer.send(Modules.TRAIN_MODEL, payload),
-  requestTrainModelMessage:       (payload) => ipcRenderer.invoke(Modules.TRAIN_MODEL, payload),
+  sendTrainControllerSWMessage:   (payload) => ipcRenderer.send(Modules.TRAIN_CONTROLLER_SW, payload),
+  sendTrainControllerHWMessage:   (payload) => ipcRenderer.send(Modules.TRAIN_CONTROLLER_SW, payload),
 
-  sendTrainControllerSWMessage:     (payload) => ipcRenderer.send(Modules.TRAIN_CONTROLLER_SW, payload),
-  requestTrainControllerSWMessage:  (payload) => ipcRenderer.invoke(Modules.TRAIN_CONTROLLER_SW, payload),
-
-  sendTrainControllerHWMessage:     (payload) => ipcRenderer.send(Modules.TRAIN_CONTROLLER_SW, payload),
-  requestTrainControllerHWMessage:  (payload) => ipcRenderer.invoke(Modules.TRAIN_CONTROLLER_SW, payload),
+  openFileDialog: (tag) => ipcRenderer.send('file', tag)
 });
