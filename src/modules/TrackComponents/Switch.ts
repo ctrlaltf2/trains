19cadf8406da0b932f72a1ba5c456672fb621461
chrisg564 <37278062+chrisg564@users.noreply.github.com>
2022-07-21 14:39:25 -0400

    add mmode and toggling switches

--- src/modules/TrackComponents/Switch.ts
+++ src/modules/TrackComponents/Switch.ts
@@ -14,10 +14,19 @@ export class Switch {
     this._position = outBlockLow;
   }
 
+  // Setter with boolean -- should reverse later
   set position(b) {
     if (b) {
       this._position = this.outBlockHigh;
     } else {
+      this._position = this.outBlockLow;
+    }
+  }
+
+  setPosition(to_block: number) {
+    if (to_block == this.outBlockHigh) {
+      this._position = this.outBlockHigh;
+    } else if (to_block == this.outBlockLow) {
       this._position = this.outBlockLow;
     }
   }
