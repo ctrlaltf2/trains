71a8e961119cd6b865cde58af4c680628e0cd098
Cameron Henning <cwh35@pitt.edu>
2022-08-04 16:15:44 -0400

    added some tests

--- src/__tests__/TrainControllerSW.test.js
+++ src/__tests__/TrainControllerSW.test.js
@@ -6,22 +6,65 @@ test('Train Controller exists', () => {
   expect(TrainControl).toBeTruthy();
 });
 
-test('Train Controller checks suggested speed bounds', () => {
+test('Train Controller emergency brake works', () => {
 
+  TrainControl.emergencyBrake();
+  expect(TrainControl.trainAttributes[1].emergencyButton).toEqual(true);
 
-
-  let suggestedSpdover = 75;
-
-  TrainControl.handleCurrentSpeedChange(suggestedSpdover);
-  expect(TrainControl.trainAttributes[1].suggestedSpeed).toBe(43);
-
-  let suggestedSpdunder = -5;
-
-  TrainControl.handleCurrentSpeedChange(suggestedSpdunder);
-  expect(TrainControl.trainAttributes[1].suggestedSpeed).toBe(0);
-
-  let suggestedSpdmiddle = 30;
-
-  TrainControl.handleCurrentSpeedChange(suggestedSpdmiddle);
-  expect(TrainControl.trainAttributes[1].suggestedSpeed).toBe('19');
 });
+test('Train Controller service brake works', () => {
+
+  TrainControl.toggleServiceBrake();
+  expect(TrainControl.trainAttributes[1].brakeStatus).toEqual(true);
+
+});
+test('Train Controller left doors open', () => {
+
+  TrainControl.openLeftDoors();
+  expect(TrainControl.trainAttributes[1].leftDoors).toEqual(false);
+
+});
+test('Train Controller right doors open', () => {
+
+  TrainControl.openRightDoors();
+  expect(TrainControl.trainAttributes[1].rightDoors).toEqual(false);
+
+});
+test('Train Controller train lights turn on', () => {
+
+  TrainControl.trainLightsOnOff();
+  expect(TrainControl.trainAttributes[1].trainLights).toEqual(false);
+
+});
+test('Train Controller cabin lights turn on', () => {
+
+  TrainControl.cabinLightsOnOff();
+  expect(TrainControl.trainAttributes[1].cabinLights).toEqual(false);
+
+
+});
+test('Train Controller activates manual mode', () => {
+
+  TrainControl.toggleAutomatic();
+  expect(TrainControl.trainAttributes[1].automaticMode).toEqual(false);
+
+});
+test('Train Controller receives brake failure', () => {
+
+  TrainControl.brakeFailure(true);
+  expect(TrainControl.trainAttributes[1].breakFail).toEqual(true);
+
+});
+test('Train Controller receives engine failure', () => {
+
+  TrainControl.engineFailure(true);
+  expect(TrainControl.trainAttributes[1].engineFail).toEqual(true);
+
+});
+test('Train Controller receives signal pickup failure', () => {
+
+  TrainControl.signalPickupFailure(true);
+  expect(TrainControl.trainAttributes[1].signalPickupFail).toEqual(true);
+
+});
+
