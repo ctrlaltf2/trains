29551b6220b1e448848790036b1cc3dfbbc8cd02
cag185 <cag185@pitt.edu>
2022-08-03 20:48:43 -0400

    case sensitive

--- src/modules/TrackModel.jsx
+++ src/modules/TrackModel.jsx
@@ -245,8 +245,8 @@ class TrackModel extends React.Component {
     this.checkTrackHeaters(redEVTemp, greenEVTemp);
 
     //  make the default block occupancy unoccupied
-    redBlocks.forEach((a) => {a.Occupancy = false});
-    greenBlocks.forEach((b) => {b.Occupancy = false});
+    redBlocks.forEach((a) => {a.occupancy = false});
+    greenBlocks.forEach((b) => {b.occupancy = false});
 
     //  send the track controller an empty set of arrays (block unoccupied)
     window.electronAPI.sendTrackControllerMessage({
@@ -297,7 +297,7 @@ class TrackModel extends React.Component {
     this.setState({
       elevation: elevationImperial,
     });
-    this.state.blockOccupancy = this.state.blocks[this.state.blockIndex].Occupancy;
+    this.state.blockOccupancy = this.state.blocks[this.state.blockIndex].occupancy;
   };
 
   loadFile = (event) => {
@@ -675,7 +675,7 @@ class TrackModel extends React.Component {
       //  check if the train is moving
       if (IS_TRAIN_MOVING) {
         //  occupy the current block
-        blocks[currBlock].Occupancy = true;
+        blocks[currBlock].occupancy = true;
 
         //  send the track model a message about the blocks being updated
         //  Tracks block occupancy
@@ -783,9 +783,9 @@ class TrackModel extends React.Component {
         //  check if the old block has been vacatted
         if (EXITED_BLOCK) {
           //  loop through blocks, first occupied block becomes unoccupied
-          const searchInd = blocks.Occupancy.indexOf.true;
+          const searchInd = blocks.occupancy.indexOf.true;
           //  set occupancy to false
-          blocks[searchInd].Occupancy = false;
+          blocks[searchInd].occupancy = false;
           //  send the track model a message about the blocks being updated
         //  Tracks block occupancy
           if(trackLine === 'Green' && greenSendingTrackCircuit === "enabled")
